5c55aa0bbfcb0a99f9f3ed32cbdcfa3a18f80a19
David Chu <thedavidchu@gmail.com>
2020-12-22 21:34:15 -0500

    Fixed bug in argument pass

--- starship/fuel_tank.py
+++ starship/fuel_tank.py
@@ -64,36 +64,6 @@ class FuelTank(object):
         else:
             return fuel
 
-    """"""
-    # def check_fuel(self, fuel: int = 1, dt: float = 1):
-    #     """
-    #     DEPRECIATED
-    #
-    #     Takes fuel request and returns the maximum amount of fuel useable.
-    #
-    #     :param fuel:
-    #     :return:
-    #     """
-    #     raise PendingDeprecationWarning
-    #
-    #     # Check flow
-    #     if fuel > self.max_flow * dt:
-    #         warnings.warn('ERROR: Requested more than max flow')
-    #         m = self.max_flow * dt
-    #     elif fuel <= 0:
-    #         warnings.warn('ERROR: Requested backward flow')
-    #         return 0
-    #     else:
-    #         m = fuel
-    #
-    #     # Check fuel levels
-    #     if fuel > self.fuel:
-    #         warnings.warn('ERROR: You wanted to use more fuel than we have!')
-    #         m = self.fuel
-    #
-    #     return m
-    """"""
-
     # ============================== REFUEL ============================== #
     def max_refuel(self, fuel: int = None, dt: float = 1):
         """
@@ -112,37 +82,6 @@ class FuelTank(object):
         else:
             return fuel
 
-    """"""
-    # def check_refuel(self, fuel, dt: float = 1):
-    #     """
-    #     DEPRECIATED
-    #
-    #     Check how much we can refuel the fuel tank.
-    #
-    #     :param fuel: int - amount of fuel we want to put in the tank
-    #     :return: int - the amount of fuel we managed to fit in the tank
-    #     """
-    #     raise PendingDeprecationWarning
-    #
-    #     # Check flow
-    #     if fuel > self.max_flow * dt:                # Cap at max flow
-    #         warnings.warn(f'ERROR: Tried to refuel too quickly. Capped at {self.max_flow}')
-    #         m = self.max_flow * dt
-    #     elif fuel <= 0:                         # Ensure positive flow
-    #         warnings.warn('ERROR: Tried to refuel negative fuel!')
-    #         return 0, 0
-    #     else:                                   # Accept
-    #         m = fuel
-    #
-    #     # Check overfill
-    #     empty = self.max_fuel - self.fuel
-    #     if fuel > empty:
-    #         warnings.warn(f'ERROR: Overflow warning. Fuel capped at {empty}')
-    #         m = empty
-    #
-    #     return m
-    """"""
-
     def refuel(self, fuel, dt: float = 1):
         """
         Refuel in time step.
@@ -151,7 +90,7 @@ class FuelTank(object):
         :param dt: float - time step to refill in
         :return: None
         """
-        max_refuel = self.max_refuel(dt)
+        max_refuel = self.max_refuel(dt=dt)
         if fuel > max_refuel:
             warnings.warn('ERROR: You spilled some fuel!')
             self.fuel += max_refuel
